c484350be47dff44b203bf0b80db93bfd34d1301
egorastapchyk <41088004+egorastapchyk@users.noreply.github.com>
2020-11-06 14:22:38 +0300

    last version

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,32 @@
-'use strict'
+'use strict';
+
+const menuButton = document.querySelector('.menu-button');
+const header = document.querySelector('.header');
+const star = document.querySelectorAll('.star');
+
+let count = 0;
+star.forEach((el) => {
+  el.style.animationDelay = `${(count += 0.2)}s`;
+});
+
+menuButton.addEventListener('click', function () {
+  header.classList.toggle('header-active');
+  menuButton.classList.toggle('menu-button-active');
+});
 var mySwiper = new Swiper('.swiper-container', {
-    slidesPerView: 2,
-    loop: true,
-    navigation: {
+  slidesPerView: 1,
+  loop: true,
+  navigation: {
     nextEl: '.arrow',
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    540: {
+      slidesPerView: 2,
+    },
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
   },
 });
